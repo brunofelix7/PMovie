71e67f9d92b81e4b5d3d859e90c5941f1a85de4d
Bruno <brunofelix.dev@gmail.com>
2024-06-29 13:29:50 -0300

    :sparkles: MoviewPopularViewModel created

---
+++ app/src/main/java/me/brunofelix/pmovie/feature/movie/presentation/state/MovieState.kt
@@ -0,0 +1,10 @@
+package me.brunofelix.pmovie.feature.movie.presentation.state
+
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+import me.brunofelix.pmovie.core.domain.model.Movie
+
+data class MovieState(
+    val movies: Flow<PagingData<Movie>> = emptyFlow(),
+)
