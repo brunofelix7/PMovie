87f84d2bf8744e862654f6abb65464132a3f9fcd
Bruno <brunofelix.dev@gmail.com>
2024-06-29 16:04:26 -0300

    :speech_balloon: Strings updated

--- app/src/main/java/me/brunofelix/pmovie/feature/movie/presentation/components/MovieContent.kt
+++ app/src/main/java/me/brunofelix/pmovie/feature/movie/presentation/components/MovieContent.kt
@@ -11,10 +11,11 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
+import me.brunofelix.pmovie.R
 import me.brunofelix.pmovie.core.domain.model.Movie
 import me.brunofelix.pmovie.core.presentation.components.ErrorView
 import me.brunofelix.pmovie.core.presentation.components.LoadingView
@@ -51,35 +52,27 @@ fun MovieContent(
             paging.apply {
                 when {
                     loadState.refresh is LoadState.Loading -> {
-                        item(
-                            span = { GridItemSpan(maxLineSpan) }
-                        ) {
+                        item(span = { GridItemSpan(maxLineSpan) }) {
                             LoadingView()
                         }
                     }
                     loadState.append is LoadState.Loading -> {
-                        item(
-                            span = { GridItemSpan(maxLineSpan) }
-                        ) {
+                        item(span = { GridItemSpan(maxLineSpan) }) {
                             LoadingView()
                         }
                     }
                     loadState.refresh is LoadState.Error -> {
-                        item(
-                            span = { GridItemSpan(maxLineSpan) }
-                        ) {
+                        item(span = { GridItemSpan(maxLineSpan) }) {
                             ErrorView(
-                                message = "Please, check or internet connection.",
+                                message = stringResource(R.string.connection_error),
                                 onRetry = { retry() }
                             )
                         }
                     }
                     loadState.append is LoadState.Error -> {
-                        item(
-                            span = { GridItemSpan(maxLineSpan) }
-                        ) {
+                        item(span = { GridItemSpan(maxLineSpan) }) {
                             ErrorView(
-                                message = "Oops!",
+                                message = stringResource(R.string.error),
                                 onRetry = { retry() }
                             )
                         }
@@ -88,10 +81,4 @@ fun MovieContent(
             }
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun MovieContentPreview() {
-
 }
--- app/src/main/java/me/brunofelix/pmovie/feature/movie/presentation/view/MoviePopularScreen.kt
+++ app/src/main/java/me/brunofelix/pmovie/feature/movie/presentation/view/MoviePopularScreen.kt
@@ -1,6 +1,5 @@
 package me.brunofelix.pmovie.feature.movie.presentation.view
 
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
