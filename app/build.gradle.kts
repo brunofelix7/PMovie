03fb287a2b32d3cfcc6ee2f07b98087f3e03098d
Bruno <brunofelix.dev@gmail.com>
2024-12-02 15:43:41 -0300

    :white_check_mark: Configuring HiltTestRunner in the project and injecting Room Tests Database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "dev.brunofelix.pmovie.HiltTestRunner"
 
         vectorDrawables {
             useSupportLibrary = true
---
+++ app/src/androidTest/java/dev/brunofelix/pmovie/di/RoomModuleTest.kt
@@ -0,0 +1,23 @@
+package dev.brunofelix.pmovie.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import dev.brunofelix.pmovie.core.data.local.MovieDatabase
+import javax.inject.Named
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RoomModuleTest {
+
+    @Provides
+    @Named("test_db")
+    fun provideInMemoryDb(@ApplicationContext context: Context) =
+        Room.inMemoryDatabaseBuilder(context, MovieDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+}
